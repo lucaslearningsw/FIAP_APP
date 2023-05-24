c26a9c075bd6e85f0272e5a16f50c21147279c40
Lucas Alixandre Lima <lucaslearningsw@gmail.com>
2023-05-24 13:06:58 -0300

    created solution

---
+++ FIAP_APP/FIAP_APP.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIAP_APP.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
