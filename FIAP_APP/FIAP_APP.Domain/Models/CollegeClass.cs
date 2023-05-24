e32262907c637a861e4e199482554e4840f7dc4f
Lucas Alixandre Lima <lucaslearningsw@gmail.com>
2023-05-24 13:34:34 -0300

    created domain class in app

---
+++ FIAP_APP/FIAP_APP.Domain/Models/CollegeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_APP.Domain.Models
+{
+    public  class CollegeClass : Entity
+    {
+        public int Curso_Id { get; set; }
+
+        public string Turma { get; set; }
+
+        public int Ano { get; set; }
+
+
+    }
+}
---
+++ FIAP_APP/FIAP_APP.Domain/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_APP.Domain.Models
+{
+    public class Student : Entity
+    {
+        public string Nome { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+    }
+}
